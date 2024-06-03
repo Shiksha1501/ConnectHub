4209361fbbab6ffe796093b9316d4f9efe9d1241
Tarushi <tarushijha97@gmail.com>
2024-06-03 19:55:31 +0530

    user registration API created

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const { chats } = require("./data/data");
 const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
 
 dotenv.config();
 
@@ -12,5 +13,6 @@ app.get("/", (req, res) => {
   res.send("API is running");
 });
 
+app.use("/api/user", userRoutes);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log("server started on PORT ${PORT}"));
