47436618b790c3b90715283c122e3ca6eb3216b8
Tarushi <tarushijha97@gmail.com>
2024-06-04 00:12:53 +0530

    errorMiddleware added

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const dotenv = require("dotenv");
 const { chats } = require("./data/data");
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
-
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 dotenv.config();
 
 connectDB();
@@ -16,5 +16,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/user", userRoutes);
+app.use(notFound);
+app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log("server started on PORT ${PORT}"));
