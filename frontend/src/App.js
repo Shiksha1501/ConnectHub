8e50e79b6c6ccaa6e2ed7b299a769c45a58aa140
Yaswitha1810 <yaswitha.1810@gmail.com>
2024-06-03 17:40:55 +0530

    initial react setup

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import { Route } from 'react-router-dom';
+import ChatPage from './Pages/ChatPage';
+import HomePage from './Pages/HomePage';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Route path="/" component={HomePage} exact></Route>
+      <Route path="/" component={ChatPage} ></Route>
+    </div>
+  );
+}
+
+export default App;
