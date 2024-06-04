4181eff5a22efb0cd9fe19aa77c773435e033650
Yaswitha1810 <yaswitha.1810@gmail.com>
2024-06-04 23:40:49 +0530

    GroupChat Frontend Done

--- frontend/src/Pages/ChatPage.js
+++ frontend/src/Pages/ChatPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Box } from "@chakra-ui/react";
 import { ChatState } from "../Context/ChatProvider";
 import SideDrawer from "../Components/Miscellaneous/SideDrawer";
@@ -7,6 +7,7 @@ import MyChats from "../Components/MyChats";
 
 const ChatPage = () => {
     const { user } = ChatState();
+    const [ fetchAgain, setFetchAgain] = useState();
 
     return (
         <div style={{width: "100%"}}>
@@ -18,8 +19,12 @@ const ChatPage = () => {
                 h = "91.5vh"
                 p="10px"
             >
-                {user && <MyChats />}
-                {user && <ChatBox />}
+                {user && (
+                    <MyChats fetchAgain={fetchAgain}/>
+                )}
+                {user && (
+                    <ChatBox fetchAgain={fetchAgain} setFetchAgain={setFetchAgain} />
+                )}
             </Box>
         </div>
         
