0f2e40d8a57b181aaf7f7452192d0546eb501318
Yaswitha1810 <yaswitha.1810@gmail.com>
2024-06-04 19:32:34 +0530

    Chats Lists Frontend

--- frontend/src/Components/Miscellaneous/SideDrawer.js
+++ frontend/src/Components/Miscellaneous/SideDrawer.js
@@ -71,6 +71,7 @@ const SideDrawer = () => {
             };
             const { data } = await axios.post(`/api/chat`, { userId }, config);
 
+            if (!chats.find((c) => c._id === data._id)) setChats([data, ...chats]);
             setSelectedChat(data);
             setLoadingChat(false);
             onClose();
